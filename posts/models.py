8be8543925834566dcd801bedaf9f0645498278f
Miguel Ramos Garcia <mframosg@eafit.edu.co>
2021-12-03 10:20:09 -0500

    migration User created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
+""" Posts models """
+# Django models
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """ User model """
+    email = models.EmailField(unique=True, max_length=100)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+    birthday = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
